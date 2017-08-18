bdd62415eb6b7180a0c210694810b89f3ebbc532
judrummer <judrummercpe@gmail.com>
2017-08-18 21:45:29 +0700

    improve UT quiz1

--- app/src/test/kotlin/com/thaikotlindev/miracleofstdlib/ch1_godlikeext/quiz1Test.kt
+++ app/src/test/kotlin/com/thaikotlindev/miracleofstdlib/ch1_godlikeext/quiz1Test.kt
@@ -1,7 +1,6 @@
 package com.thaikotlindev.miracleofstdlib.ch1_godlikeext
 
-import org.hamcrest.CoreMatchers.equalTo
-import org.hamcrest.CoreMatchers.notNullValue
+import org.hamcrest.CoreMatchers.*
 import org.junit.Assert.assertThat
 import org.junit.Test
 
@@ -14,6 +13,17 @@ class ProductDetailFragmentTest {
             arguments?.let {
                 assertThat(it.getString(ProductDetailFragment.ARG_PRODUCT_ID), equalTo("10"))
                 assertThat(it.getString(ProductDetailFragment.ARG_SOMETHING), equalTo("test"))
+            }
+        }
+    }
+
+    @Test
+    fun testMiracleInstanceNoSomething() {
+        ProductDetailFragment.miracleInstance("10").run {
+            assertThat(arguments, notNullValue())
+            arguments?.let {
+                assertThat(it.getString(ProductDetailFragment.ARG_PRODUCT_ID), equalTo("10"))
+                assertThat(it.getString(ProductDetailFragment.ARG_SOMETHING), nullValue())
             }
         }
     }
